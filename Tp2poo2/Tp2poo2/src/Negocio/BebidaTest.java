713096e6964f22a7f1e1f6c7d0d1842832cbadcb
Deilerl <deiler_l@hotmail.com>
2018-06-15 00:37:12 -0300

    add caso de teste

---
+++ Tp2poo2/Tp2poo2/src/Negocio/BebidaTest.java
@@ -0,0 +1,45 @@
+package Negocio;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class BebidaTest {
+
+	private Object nome;
+	private int id;
+
+	@Test
+	public void testGetId() {
+		id = 1;
+		System.out.println(nome);
+		
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetId() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetNome() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetNome() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetPreco() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetPreco() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ Tp2poo2/Tp2poo2/src/Persistencia/ConexaoTest.java
@@ -0,0 +1,21 @@
+package Persistencia;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ConexaoTest {
+
+	//private String pwd;
+	//private String user;
+
+	@Test
+	public void testConexao() {
+		String bd = "Restaurante";
+		String host = "127.0.0.1";
+		String user = "root";
+		String pwd = "root";
+		
+		fail("Not yet implemented");
+	}
+}
